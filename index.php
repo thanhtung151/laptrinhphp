a6d9002aaf91adfd7b209bbf9b3d2478b4d2a646
thanhtung151 <106794955+thanhtung151@users.noreply.github.com>
2023-03-07 09:56:55 +0700

    commen

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Web giày chính hãng </title>
+   
+<body>
+    <div class="wrapper">
+        <?php
+            include("header.php");
+            include("menu.php");
+            include("main.php");
+            include("footer.php");
+            
+        ?>
+      
+    
+    
+    
+    
+</body>
+</html>
